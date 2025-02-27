ea9c60ae3c1b87ce3214d4b28fbd4f593856e922
soonge2 <jiwonsoong13@gmail.com>
2025-02-27 21:48:59 +0900

    FE: [chore] 버전 변경

--- src/views/Project/Project.jsx
+++ src/views/Project/Project.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import './Project.css';
 
 export default function Project() {
     const { projectId } = useParams(); // url에서 projectId 추출
@@ -8,32 +9,41 @@ export default function Project() {
     const project = projectList.find(p => p.projectId === parseInt(projectId, 10));
 
 
+    if (!project) {
+        return <p>해당 프로젝트를 찾을 수 없습니다.</p>;
+    }
     return (
-        <div className='project'>
-            {project? (
-                <>
+        <div className='project-container'>
+            <div className='project-header'>
+                <div className='project-title'>프로젝트: {project.projectName}</div>
+                <button className='download-button'>
+                    <img src='/images/downloadicon.png' alt='download' style={{ width: "50px", height: "auto" }}/>
+                </button>
+            </div>
+            <div className='project-content'>
+                {/* 질문 카드 */}
+                <div className='questionCard'>
+                    <div className='question-content'>
+                        <div className='arrow-left'>{"<"}</div>
+                        <div className='question-wrapper'>
+                            문제 질문
+                            체크 
+                            답적는 칸
+                        </div>
+                        <div className='arrow-right'>{">"}</div>
+                    </div>
+                    <button className='submit-answer-button'>정답 제출</button>
+                </div>
+                {/* 정답 카드 */}
+                <div className='answerCard'>
                     <div>
-                        <div>프로젝트: {project.projectName}</div>
-                        <div>다운로드 버튼</div>
+                        정답
                     </div>
-                    <div>얘가 스크롤 돼야함
-                        <div className='questionCard'>
-                            <div className='qustionContent'>
-                                <div>왼쪽 화살표</div>
-                                <div>문제 래퍼</div>
-                                <div>오른쪽 화살표</div>
-                            </div>
-                            <div>정답 제출 버튼</div>
-                        </div>
-                        <div className='answerCard'>
-                            <div>내용</div>
-                        </div>
+                    <div>
+                        해설
                     </div>
-                </>
-            ) : (
-                <p>해당 프로젝트를 찾을 수 없습니다.</p>
-            )
-            }
+                </div>
+            </div>
         </div>
     )
 }
